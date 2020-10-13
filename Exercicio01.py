110058fc7255de63db1611ca2a00d52cf696895c
DedeApenas <andrelromao@hotmail.com>
2020-10-13 20:32:56 -0300

    fix: removi o while

--- Exercicio01.py
+++ Exercicio01.py
@@ -1,10 +1,9 @@
 lista = [1,2,3,4]
 def rotacionar(lst,x):
-    copy = list(lst)
-    while numero !=0:   
-        for i in range(len(lst)):
-            if x<0:
-                lst[i+x] = copy[i]
+    copy = list(lst) 
+    for i in range(len(lst)):
+        if x<0:
+            lst[i+x] = copy[i]
         else:
             lst[i] = copy[i-x]   
 numero = int(input("Digite um numero POSITIVO ou NEGATIVO: "))
